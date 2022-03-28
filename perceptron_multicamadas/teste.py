563db5b7bdc604194de13a970a9165e532ff7775
Vitor Vaz Andrade <vitorandrade@ecomp.uefs.br>
2022-03-27 23:38:38 -0300

    updates

--- perceptron_multicamadas/teste.py
+++ perceptron_multicamadas/teste.py
@@ -1,7 +1,7 @@
 import math
 import numpy as np
 
-a = np.array([[0, 1, 2]])
-b = np.array([[2, 2, 2], [2, 2, 2], [2, 2, 2]])
-
-print(a.T*b)
+a = np.array([[0, 1, 2], [2, 2, 2]])
+b = np.array([2,2,2])
+np.insert(b, 0, -1)
+print(b)
