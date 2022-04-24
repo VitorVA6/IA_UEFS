84a3fbfb3a3d57dec4c7a8fa4d57e0f564e51462
Vitor Vaz Andrade <vitorandrade@ecomp.uefs.br>
2022-04-24 00:22:31 -0300

    kohonen update

--- kohonen/kohonen.py
+++ kohonen/kohonen.py
@@ -7,11 +7,20 @@ def read_file(name_file):
     file = open(name_file, 'r')
     dados = file.read().split('\n')[9:]
     dados = [item.split(', ') for item in dados]
-    x = [list(map(float, item[0:4])) for item in dados]
-    for item in x:
-        item.insert(0, -1)
-    x = np.array(x)
+    x = np.array([list(map(float, item[0:4])) for item in dados])
+    x[0:, 0] = normalize(x[0:, 0], 4.3, 7.9)
+    x[0:, 1] = normalize(x[0:, 1], 2.0, 4.4)
+    x[0:, 2] = normalize(x[0:, 2], 1.0, 6.9)
+    x[0:, 3] = normalize(x[0:, 3], 0.1, 2.5)
     return x
+
+def normalize(arr, minimo, maximo):
+    norm_arr = []
+    diff_arr = maximo - minimo    
+    for i in arr:
+        temp = ((i - minimo)/diff_arr)
+        norm_arr.append(temp)
+    return np.array(norm_arr)
 
 def vizinhos():
     viz = []
@@ -34,14 +43,17 @@ def vizinhos():
 
 def kohonen():
     x = read_file('dataset/iris-10-1tra.dat')
-    w = np.array([[[random.random() for _ in range(5)] for _ in range(5)]for _ in range(5)])
+    w = np.array([[[random.random() for _ in range(4)] for _ in range(5)]for _ in range(5)])
     vizinhanca = vizinhos()
     n = 0.01
     epocas = 0
+    print(x)
 
 def teste():
-    lista = [5, 0]
-    if 5 in lista:
-        print('tem')
+    lista = np.array([[5., 0., 2.],[4., 2., 1.]])
+    otimos = lista[0:, 0]/4.6
+    print(otimos)
+    lista[0:, 0] = otimos
+    print(lista)
 
 kohonen()
