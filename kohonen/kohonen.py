3ac01c54b1e2f4b986adf1276527614f9d873477
Vitor Vaz Andrade <vitorandrade@ecomp.uefs.br>
2022-04-23 21:31:23 -0300

    vizinhança update

--- kohonen/kohonen.py
+++ kohonen/kohonen.py
@@ -13,21 +13,35 @@ def read_file(name_file):
     x = np.array(x)
     return x
 
-def kohonen():
-    x = read_file('dataset/iris-10-1tra.dat')
-    w = np.array([[[random.random() for _ in range(5)] for _ in range(5)]for _ in range(5)])
-    vizinhos = [[[0, 1], [1, 0]] ,[[0, 0], [1, 1], [2, 0]]]
-    print(w)
-
 def vizinhos():
     viz = []
     for l in range(5):
         for c in range(5):
-            viz.append([[l, c+1], [l,c-1], [l+1, c], [l-1, c]])
-    viz = np.array(viz)
-    indices = np.where(viz==-1)[0]
-    indices2 = np.where(viz==-1)[1]
-    viz = np.delete(viz, )
-    
+            viz.append([[l,c-1], [l, c+1] ,  [l-1, c], [l+1, c]])   
+    for e in viz:
+        for el in e:
+            if(-1 in el):
+                indice1 = viz.index(e)
+                indice2 = e.index(el)
+                del(viz[indice1][indice2])
+    for e in viz:
+        for el in e:
+            if 5 in el:
+                indice1 = viz.index(e)
+                indice2 = e.index(el)
+                del(viz[indice1][indice2])
+    return viz
 
-vizinhos()
+def kohonen():
+    x = read_file('dataset/iris-10-1tra.dat')
+    w = np.array([[[random.random() for _ in range(5)] for _ in range(5)]for _ in range(5)])
+    vizinhanca = vizinhos()
+    n = 0.01
+    epocas = 0
+
+def teste():
+    lista = [5, 0]
+    if 5 in lista:
+        print('tem')
+
+kohonen()
